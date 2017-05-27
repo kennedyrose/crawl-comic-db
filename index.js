9efe60555f8305c8753bbe59b648991fa9bd5e0d
Kennedy Rose <kennedy@kennedyrose.com>
2017-05-27 10:35:02 -0500

    Promise based

--- index.js
+++ index.js
@@ -2,46 +2,44 @@
 require('dotenv').config({ silent: true })
 const Nightmare = require('nightmare')
 
-module.exports = (opt) => {
-	opt = Object.assign({
-		stopId: '11505577'
-	}, opt)
-	const nightmare = Nightmare({
-		show: false,
-		alwaysOnTop: false,
-		openDevTools: {
-			mode: 'detach'
-		}
-	})
-	nightmare
-		.goto(`https://cloud.collectorz.com/311694/comics?viewType=list`, {
-			method: 'POST'
+// Get all new IDs
+function getNewIds(opt){
+	return new Promise((resolve, reject) => {
+		const nightmare = Nightmare({
+			show: false,
+			alwaysOnTop: false,
+			openDevTools: {
+				mode: 'detach'
+			}
 		})
-		.wait('.x-collection')
-		.click('#sortDropdown')
-		.select('[name="editView"] select.form-control', 'AddedDate')
-		.select('[name="editView"] [name="order"]', 'DESC')
-		.click('[for="submit-button-sort-mobile"]')
-		.wait('.x-collection')
-		.evaluate(getList, opt)
-		.end()
-		.then(console.log)
-		.catch(console.error)
+		nightmare
+			.goto(`https://cloud.collectorz.com/311694/comics?viewType=list`, {
+				method: 'POST'
+			})
+			.wait('.x-collection')
+			.click('#sortDropdown')
+			.select('[name="editView"] select.form-control', 'AddedDate')
+			.select('[name="editView"] [name="order"]', 'DESC')
+			.click('[for="submit-button-sort-mobile"]')
+			.wait('.x-collection')
+			.evaluate(getList, opt)
+			.end()
+			.then(ids => {
+				opt.ids = ids
+				resolve(opt)
+			})
+			.catch(reject)
+	})
 }
-
 function getList(opt, done){
 	var timeout = 3000
 	var arr = []
 	var id
-
 	// Continuous scrolling
-	function scroll(){
+	setInterval(function(){
 		document.body.scrollTop += 10000
-	}
-	setInterval(scroll, 500)
-
-
-
+	}, 500)
+	// Get IDs
 	var timeoutProgress = 0
 	var els
 	var cursor = 0
@@ -67,7 +65,29 @@ function getList(opt, done){
 		setTimeout(getBatch, 50)
 	}
 	getBatch()
-
 }
+
+
+
+// Get comic info from IDs
+function getInfo(opt){
+	
+}
+
+
+module.exports = opt => {
+	return new Promise((resole, reject) => {
+		// Default options
+		opt = Object.assign({
+			stopId: '11505577'
+		}, opt)
+		// Go!
+		getNewIds(opt)
+			.then(getInfo)
+			.then(console.log)
+			.catch(console.error)
+	})
+}
+
 
 module.exports()
